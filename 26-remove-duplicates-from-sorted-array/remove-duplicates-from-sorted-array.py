26dcec7becccfe0bfd5c0a6121977204122ac313
MASANAM SRI LAKSHMI BHANU PRAKASH <bhanuprakashmasanam2003@gmail.com>
2026-02-21 22:03:27 +0530

    Time: 4 ms (41.70%) | Memory: 14 MB (43.98%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -0,0 +1,13 @@
+class Solution :
+    def removeDuplicates(self,nums):
+        j=0
+        for i in range(1,len(nums)):
+
+            if nums[i] != nums[j]:
+                j+=1
+                nums[j]=nums[i]
+        return j+1        
+
+        
+         
+        

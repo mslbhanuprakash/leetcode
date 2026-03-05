11e112142ac354699abdf81ab4bf48ba3bd12e19
MASANAM SRI LAKSHMI BHANU PRAKASH <bhanuprakashmasanam2003@gmail.com>
2026-03-05 19:28:01 +0530

    Time: 19 ms (46.11%) | Memory: 14.1 MB (29.17%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,8 +1,15 @@
 class Solution(object):
     def isPalindrome(self, s):
         s1=''.join (ch.lower() for ch in s if ch.isalnum())
-        return s1==s1[::-1]
-       
+        left,right=0,len(s1)-1
+        while left<right:
+            if s1[left]!=s1[right]:
+                return False
+            left+=1
+            right-=1
+
+      
+        return True       
 
 
        

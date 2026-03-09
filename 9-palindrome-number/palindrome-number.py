8483174e2e4a96de447f507e2f80a630d26c7dbe
MASANAM SRI LAKSHMI BHANU PRAKASH <bhanuprakashmasanam2003@gmail.com>
2026-03-09 15:26:13 +0530

    Time: 8 ms (71.36%) | Memory: 12.5 MB (17.04%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,6 +1,15 @@
 class Solution(object):
     def isPalindrome(self, x):
-        res=str(x)
-        return res == res[::-1]
         
-            
+        temp=x
+        rev=0
+        while x>0:
+            d=x%10
+            rev=rev*10+d
+            x=x//10
+
+        return rev ==temp
+    
+       
+    
+        

0b0659a0f44166c73ca27e8f08fd0510b894d5ce
MASANAM SRI LAKSHMI BHANU PRAKASH <bhanuprakashmasanam2003@gmail.com>
2026-03-09 20:09:27 +0530

    Time: 11 ms (5.93%) | Memory: 12.4 MB (83.60%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPerfectSquare(self, num):
+        i=1
+        while i*i<=num:
+            if i*i ==num:
+                return True
+            i+=1
+        return False
+     
+        

7dd323c201f12faba0395adc8018dc74933108a3
MASANAM SRI LAKSHMI BHANU PRAKASH <bhanuprakashmasanam2003@gmail.com>
2026-07-16 14:18:30 +0530

    Time: 3 ms (37.40%) | Memory: 13.1 MB (53.97%) - LeetSync

---
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution(object):
+    def levelOrder(self, root):
+        if root is None:
+            return []
+        q=deque()
+        q.append(root)
+        ans=[]
+        while q:
+            level=[]
+            size=len(q)
+            for i in range(size):
+                node=q.popleft()
+                level.append(node.val)
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            ans.append(level)
+        return ans
+        
+        
